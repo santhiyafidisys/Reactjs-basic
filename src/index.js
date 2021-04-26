07161b6d105a4c46a1d6b57adefd7e1fe038bd0a
santhiya <santhiya2712@gmail.com>
2021-04-26 19:38:20 +0530

    change in style

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import Box1 from './Box1.js';
 import Box2 from './Box2.js';
 import Button from './Button.js';
 
-
-
 class Parents extends React.Component {
   // function changeValue {
   //   return(
@@ -14,30 +12,22 @@ class Parents extends React.Component {
   //   )
     
   // }
-
- 
  constructor(props){
     super(props)
     this.state={value:"update"}
    }
      changevalue = () =>
- {
+ 		{
  this.setState({value:"update"})
- }
-
- 
-
+ 		}
   render() 
   {
-  
       return  <div  className={styles.parentbox}>
         <h5 className={styles.parent}>Container component </h5>
         <Box1 container1="Box1 title"  />
         <Box2  container2="Box2 title"/>
         <Button />
-        
-        </div>
-
+      </div>
   }
 }
 
